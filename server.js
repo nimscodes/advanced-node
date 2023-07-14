030fb88d715e6c6498f4e7d0b8124cc18f23709b
nimscodes <princenimako28@gmail.com>
2023-07-14 01:27:38 +0100

    another obj

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const passport = require('passport');
 const session = require('express-session');
-const { ObjectID } = require('mongodb')
+const { ObjectID } = require('mongodb');
+const LocalStrategy = require('passport-local');
 
 const app = express();
 
@@ -43,6 +44,16 @@ myDB(async client => {
       done(null, doc);
     })
   })
+
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) return done(err)
+      if (!user) return done(null, false);
+      if (password !== user.password) return done(null, false);
+      return done(null, user)
+    })
+  }))
 
 }).catch(e => {
   app.route('/').get((req,res) => {
